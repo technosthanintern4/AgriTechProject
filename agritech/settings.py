20e96dc1feed484770662153ea1edc7147af32d4
technosthanintern4 <technosthan.intern.4@gmail.com>
2026-06-12 16:31:41 +0530

    Configured Cloudinary media storage

--- agritech/settings.py
+++ agritech/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'cloudinary',
+    'cloudinary_storage',
 
     'accounts',
     'core',
@@ -99,7 +100,10 @@ cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
     api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-)
+    DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+    MEDIA_URL = "/media/"
+    )
 
 # INTERNATIONALIZATION
 LANGUAGE_CODE = 'en-us'
