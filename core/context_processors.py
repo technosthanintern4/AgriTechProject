028b2eea9331b23db0595b155bd0d80144b8dc34
technosthanintern4 <technosthan.intern.4@gmail.com>
2026-06-12 15:01:18 +0530

    Added SiteSettings model

---
+++ core/context_processors.py
@@ -0,0 +1,10 @@
+from .models import SiteSettings
+
+
+def site_settings(request):
+
+    settings = SiteSettings.objects.first()
+
+    return {
+        'site_settings': settings
+    }
--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class SiteSettings(models.Model):
+
+    background_image = models.ImageField(
+        upload_to='backgrounds/'
+    )
+
+    def __str__(self):
+        return "Website Settings"
