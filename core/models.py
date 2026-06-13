1c785cb9d54afe92880b8a6ebc34abc0f1ce7062
technosthanintern4 <technosthan.intern.4@gmail.com>
2026-06-13 16:52:31 +0530

    update front page access to admin

--- core/models.py
+++ core/models.py
@@ -7,5 +7,12 @@ class SiteSettings(models.Model):
         upload_to='backgrounds/'
     )
 
+    background_video = models.FileField(
+        upload_to='backgrounds/',
+        blank=True,
+        null=True,
+        help_text='Upload a background video (mp4). If present, video will be shown instead of image.'
+    )
+
     def __str__(self):
         return "Website Settings"
