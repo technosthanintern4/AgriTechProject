474b896caa63c8206a30369540eac0b35f0b4eee
technosthanintern4 <technosthan.intern.4@gmail.com>
2026-06-13 14:44:17 +0530

    book consultations email

--- consultations/admin.py
+++ consultations/admin.py
@@ -1,9 +1,48 @@
 from django.contrib import admin
+from django.core.mail import send_mail
+from django.conf import settings
+from django.utils import timezone
+
 from .models import Consultation
+
+
+@admin.action(description="Send confirmation email to selected users")
+def send_confirmation_email(modeladmin, request, queryset):
+
+    for consultation in queryset:
+
+        if consultation.confirmation_method == "email":
+
+            message = consultation.confirmation_message
+
+            if not message:
+
+                message = (
+                    f"Hello {consultation.user.username},\n\n"
+                    f"Your appointment with Dr. {consultation.doctor.name} "
+                    f"has been confirmed.\n\n"
+                    f"Appointment Date: {consultation.appointment_date}\n\n"
+                    f"Thank you,\n"
+                    f"AgriTech Nursery"
+                )
+
+            send_mail(
+                subject="Appointment Confirmation",
+                message=message,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[consultation.user.email],
+                fail_silently=False
+            )
+
+            consultation.confirmation_sent = True
+            consultation.confirmed_at = timezone.now()
+            consultation.save()
 
 
 @admin.register(Consultation)
 class ConsultationAdmin(admin.ModelAdmin):
+
+    actions = [send_confirmation_email]
 
     list_display = [
         'user',
