320c99b1be19863df6dabc97ab5f2acf7ff743b9
technosthanintern4 <technosthan.intern.4@gmail.com>
2026-06-16 14:28:32 +0530

    services home pr

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from services.models import Service
 
 def home(request):
-    services = Service.objects.filter(is_active=True)[:3]
+    services = Service.objects.filter(is_active=True)
     return render(request, 'core/home.html', {'services': services})
